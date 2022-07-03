f9c7a64a97443c24d3ba85c24a0cddf42d62eceb
Clintonkes <clintonemeka05@gmail.com>
2022-07-02 17:54:57 -0700

    divide

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,11 +1,11 @@
-#/usr/bin/python3
+#!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    check_list = []
+    check_div = []
 
-    for next_num in range(len(my_list)):
+    for i in range(len(my_list)):
         if my_list[i] % 2 == 0:
-            check_list.append(True)
+            check_div.append(True)
         else:
-            check_list.append(False)
+            check_div.append(False)
 
-    return(check_list)
+    return (check_div)
